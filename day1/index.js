ccf38850994a384231e29c5c0960b9a5b3cc5e6b
Maithali <maithalygharde1997@gmail.com>
2023-01-21 17:31:44 +0530

    First commit

---
+++ day1/index.js
@@ -0,0 +1,15 @@
+//want to print something use console.log
+console.log("HI there i am Maithaly ");
+
+//want to perform multiplication operation
+console.log(2*2);
+
+//want to perform addition
+console.log(2+2);
+
+//the most important thing for us is data
+//data need to be stored
+//store data in temporary 
+//store no 3
+num = 3;
+console.log(num);
