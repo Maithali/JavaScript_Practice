102308cece9fae053e2cff510390231e1f583e51
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-21 19:05:01 +0530

    Add files via upload

--- day1/index2.js
+++ day1/index2.js
@@ -1,9 +1,40 @@
-num=7;//number
+"use strict"
+let num=7;//number
 //num=seven;//string //error seven is not define
-num="seven";//whenever  you use string use " " or '' or `
+//whenever  you use string use " " or '' or `
 
 
-numberWhichIWantTOWorkWith=10;
-numberWhichIWantTOWorkWIth=9;
+let numberWhichIWantTOWorkWith=10;
+let numberWhichIWantTOWorkWIth=9;
 console.log(numberWhichIWantTOWorkWith);
+
+let num2 = 2;//let is a way of declaring a variable
+let name1="maithaly";
+console.log(name1);
+
+//statically type language
+//dynamically type language-javascript
+
+//operations 
+let numb1=6;
+let numb2=1;
+console.log(numb1 + numb2);
+console.log(numb1 - numb2);
+console.log(numb1 * numb2);
+console.log(numb1 / numb2);
+console.log(numb1 % numb2);
+
+
+//print value and type of
+let x=6;
+console.log(x);
+console.log(typeof x);
+
+
+let user="maithaly"
+console.log(user, typeof user);
+
+let value="7879";
+console.log(value,typeof value);
+
 
