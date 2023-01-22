e9de1d00af586bedbe5bdc5e7b1067f993ffc94c
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-22 20:37:32 +0530

    Add files via upload

---
+++ index8.js
@@ -0,0 +1,26 @@
+
+//function
+function add(n1,n2){
+return n1 + n2;
+}
+let result=add(5,6);
+console.log(result);
+
+//anonymus function
+let add1 = function(){
+    console.log("in Add1");
+}
+add1();
+//function
+let sub =function(n1,n2){
+    return n1 - n2;
+}
+let resultsub = sub(85,56);
+console.log(resultsub);
+
+//arrow function
+let ad = (n1,n2) =>{
+    return n1 + n2;
+}
+let re = ad(8,9);
+console.log(re);
