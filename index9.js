401b958c14f8697baf53d069db1b31c03a1c4f9d
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-23 19:28:41 +0530

    Add files via upload

---
+++ index9.js
@@ -0,0 +1,22 @@
+//objects
+let student = new Object();
+student.name="maithaly";
+student.age=25;
+student.roll_no=90;
+student.yop=2020;
+student.subjects=["blockchain,Javascript,React"];
+console.log(student);
+student.marks = [20,63,58];
+console.log(student);
+
+//function
+function Student(name,age,yop,qual,dob){
+    this.name=name;
+    this.age=age;
+    this.yop=yop;
+    this.qual=qual;
+    this.dob=dob
+}
+
+let student1 = new Student();
+console.log(student1);
