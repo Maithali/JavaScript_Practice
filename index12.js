bb852558aa893cd2556138fac5293aac32170a11
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-25 10:20:39 +0530

    adding file

--- index12.js
+++ index12.js
@@ -32,6 +32,7 @@ class PersonDo{
     }
     greet(){
         console.log(this.name);
+        console.log(`occupation then :${this.occupation}`);
     }
 
 }
@@ -42,8 +43,8 @@ class Employee extends PersonDo{
         this.occupation="Blockchain developer"
     }
     greet(){
-        console.log(`Hello Student : ${this.name}`);
-        console.log(`occupation isnow : ${this.occupation}`);
+        console.log(`Hello Employee : ${this.name}`);
+        console.log(`occupation is now : ${this.occupation}`);
     }
 }
 
@@ -51,3 +52,23 @@ let obj1 = new PersonDo("Maitheeeee");
 obj1.greet();
 let obj2 = new Employee("Maithaly");
 obj2.greet();
+
+
+//functional Inheritance
+
+function Animal(value){
+    var obj = {};//create an empty object
+    obj.name = value.name1;//assign a new attribuute to object
+    return obj;//return the value of object
+};
+
+function dog(value){
+    var obj=Animal(value);
+    obj.greet=function(){
+        return `Hello , ${obj.name1}`;
+    };
+    return obj;
+};
+
+var myCuteDog = dog({name1 :"Snoop"});
+console.log(myCuteDog.greet());
