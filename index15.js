13fe51ae2c5a83f9aabc0c106ddb1c40ecd5c7fb
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-25 10:23:17 +0530

    hoisting

---
+++ index15.js
@@ -0,0 +1,9 @@
+// hosting - accesing the variable,function,methods without even defining is allowed in js
+console.log(a);
+display();
+var a = 10;
+function display(){
+    console.log("maithaly and Ineruron");;
+}
+console.log(a);
+display();
