4bb6c56b99262d0f73209816861dfeafd36ee191
Maithali <121287753+Maithali@users.noreply.github.com>
2023-01-27 08:13:06 +0530

    Add files via upload

---
+++ index21.js
@@ -0,0 +1,14 @@
+//math
+let result = Math.ceil(4.5);// 5  top value
+ result = Math.floor(3.6);//3 lowest value
+ result = Math.round(3.6); //4  .5 above willgive upper .5 belowwillgive lower
+ result = Math.trunc(5.4); //5 rounding to 0
+ result = Math.max(10,20,60,30,40,);//60
+ result = Math.min(10,5,30,4,3,20);//3
+ result = Math.sqrt(9); //3  3*3
+ result = Math.cbrt(8); //2  2*2*2
+ result = Math.pow(3,2);//9 3*3
+ result = Math.abs(-5.5);//5.5 
+ result = Math.PI;//3.141592653589793
+ 
+ console.log(result);
