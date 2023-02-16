913fac049a08a5aad4fc6ce53b3655e74c357593
Maithali <121287753+Maithali@users.noreply.github.com>
2023-02-16 16:09:00 +0530

    program

---
+++ sphere.js
@@ -0,0 +1,23 @@
+// // sphere
+
+// const pi = 3.14159;//value of pi
+
+// //function to calculate Volume of sphere
+// function volume(r) {
+//     let vol = ((4)/(3))* pi* r * r* r;
+//     return vol;
+// }
+// let radius = 12;
+// let vol;
+// vol= volume(radius).toFixed(4);
+
+// console.log("Volume of Sphere:"+ vol);
+
+
+
+
+function cone(input) {
+    let [r,h] = input.map(Number);
+    let s = Math.sqrt(r*r+h*h);
+    let volume=Math
+}
